f2ce19194b7ed721119119a0e0a1a6c4ea052ab8
ihongx <30934502+ihongx@users.noreply.github.com>
2017-08-26 18:57:34 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -1,5 +1,3 @@
-
-
 <?php
 $access_token = 'LSSCN/p3/lhdozDF8tABB70GY4vMlLFBcX/oacNQoUOE4Qw9Qd5G684vwkieb0WZKaARBB5GLk1/COCvyVgge4RSunJsU6GAuuMCXOfiAYd/9wriXor92gifDvBJe/FV5mT7fcEelqg9OegXelzHEAdB04t89/1O/w1cDnyilFU=';
 
@@ -26,57 +24,10 @@ if (!is_null($events['events'])) {
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
-		
-
-		$data = [
+			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
 			];
-			
-	/*		
-//**********************************************	
-
-	$post={
-	//	"messgaes" : $messages,
-	//	"replyToken":$replyToken,
-  "type": "template",
-  "altText": "this is a buttons template",
-  "template": {
-      "type": "buttons",
-      "thumbnailImageUrl": "https://www.google.co.th/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwia6OK66vTVAhWIwI8KHQjXCKoQjRwIBw&url=https%3A%2F%2Fwww.royalcanin.com%2Fproducts%2Fdog%2Fmedium&psig=AFQjCNFC17p_SgVP0CReb4lyNZ-EvqHp1Q&ust=1503834563250208",
-      "title": "Menu",
-      "text": "Please select",
-      "actions": [
-          {
-            "type": "postback",
-            "label": "Buy",
-            "data": "action=buy&itemid=123"
-          },
-          {
-            "type": "postback",
-            "label": "Add to cart",
-            "data": "action=add&itemid=123"
-          },
-          {
-            "type": "uri",
-            "label": "View detail",
-            "uri": "http://www.google.co.th"
-          }
-      ]
-  }
-}
-
-
-
-//*************************************************************
-			
-			
-	*/
-		
-			
-			
-			
-			
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
@@ -90,24 +41,7 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
 			echo $result . "\r\n";
-			
-			
-	
-			
-			
-			
-			
-			
-			
-			
 		}
 	}
 }
-echo "OK";
-
-?>
-
-
-			
-		
-		
+echo "NOT OK";
