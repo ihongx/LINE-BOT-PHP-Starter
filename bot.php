be42dcb16bb25fdf24e6922d6ce4b3c807ebd549
ihongx <30934502+ihongx@users.noreply.github.com>
2017-08-26 20:34:23 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -18,7 +18,7 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			
-		$messages = {
+		$messages = '{
          "messages":[
           {
                "type":"text",
@@ -29,7 +29,7 @@ if (!is_null($events['events'])) {
                "text":"May I help you?"
           }
       ]
-};
+}';
 			
 
 			// Make a POST Request to Messaging API to reply to sender
