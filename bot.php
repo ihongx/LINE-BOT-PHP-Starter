63843b5c17cd955ea459735b91c77ab3e51523f8
ihongx <30934502+ihongx@users.noreply.github.com>
2017-08-26 19:40:57 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -20,27 +20,25 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
-			
-	$images = [	
-    "type"=> "image",
-    "originalContentUrl"= "https://www.google.co.th/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjom53Y8PTVAhWBp48KHaexDO8QjRwIBw&url=https%3A%2F%2Fwww.cesarsway.com%2Fdog-behavior&psig=AFQjCNE_fAT2zKTT06zKO96nIK5_Dy1sOA&ust=1503836235912612",
-    "previewImageUrl"= "https://www.google.co.th/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiUvf7i8PTVAhVCpY8KHRjSAIEQjRwIBw&url=http%3A%2F%2Fwww.powerlifetothemax.com%2F%3Fp%3D1624&psig=AFQjCNE_fAT2zKTT06zKO96nIK5_Dy1sOA&ust=1503836235912612"
-];
-			
+				
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-				'images' =>[$images]
-			];
-			$post = json_encode($data);
+			{	"replyToken":$replyToken },
+				{
+				"type" : "text",
+				"text" : $text
+				},
+				{"type" : "image",
+			"originalContentUrl" : "https://www.google.co.th/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjom53Y8PTVAhWBp48KHaexDO8QjRwIBw&url=https%3A%2F%2Fwww.cesarsway.com%2Fdog-behavior&psig=AFQjCNE_fAT2zKTT06zKO96nIK5_Dy1sOA&ust=1503836235912612",
+			 "previewImageUrl" : "https://www.google.co.th/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiUvf7i8PTVAhVCpY8KHRjSAIEQjRwIBw&url=http%3A%2F%2Fwww.powerlifetothemax.com%2F%3Fp%3D1624&psig=AFQjCNE_fAT2zKTT06zKO96nIK5_Dy1sOA&ust=1503836235912612"
+			}
+			
+			
+			]
+				
+			//$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
 			$ch = curl_init($url);
@@ -63,7 +61,7 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-echo "I am Hong";
+echo "I am IHong";
 ?>
 
 			
