067c0dc85c7253930da0d217c9707a0776fdeeda
ihongx <30934502+ihongx@users.noreply.github.com>
2017-08-26 21:02:46 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -18,24 +18,12 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			
-		$messages = [
-        
-       
-               'type'=>'text',
-               'text'=>$text
-        
-      
-    ]
-;
+
 			
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			
+					
 $post = json_encode(['replyToken' => $replyToken,
                      'messages' => [
                       'type' => 'text',
@@ -64,3 +52,4 @@ $post = json_encode(['replyToken' => $replyToken,
 	}
 }
 echo "OOKKK";
+?>
