57b6f5f93f6c36409dabb1d52d746ff37b9e55de
ihongx <30934502+ihongx@users.noreply.github.com>
2017-08-27 00:15:47 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -68,15 +68,15 @@ if (strpos($_msg, 'สอนเป็ด') !== false) {
     $arrPostData['messages'][3]['originalContentUrl'] = 'https://i.pinimg.com/736x/da/af/73/daaf73960eb5a21d6bca748195f12052--lion-photography-lion-kings.jpg';
 	$arrPostData['messages'][3]['previewImageUrl'] = 'https://i.pinimg.com/736x/da/af/73/daaf73960eb5a21d6bca748195f12052--lion-photography-lion-kings.jpg';
 		 $arrPostData['messages'][4]['type'] = "image";
-    $arrPostData['messages'][4]['originalContentUrl'] = 'https://i.pinimg.com/736x/27/55/3c/27553cb31d768ab4c51163768275efe7--by-chloe--in.jpg';
-	$arrPostData['messages'][4]['previewImageUrl'] = 'https://i.pinimg.com/736x/27/55/3c/27553cb31d768ab4c51163768275efe7--by-chloe--in.jpg';
+    $arrPostData['messages'][4]['originalContentUrl'] = "https://i.pinimg.com/736x/27/55/3c/27553cb31d768ab4c51163768275efe7--by-chloe--in.jpg";
+	$arrPostData['messages'][4]['previewImageUrl'] = "https://i.pinimg.com/736x/27/55/3c/27553cb31d768ab4c51163768275efe7--by-chloe--in.jpg";
 	
 	
-		$arrPostData['messages'][5]['type'] = 'location';
-		$arrPostData['messages'][5]['title'] = 'my location';
-		$arrPostData['messages'][5]['address'] = 'Bangkok, Thailand';
-		$arrPostData['messages'][5]['latitude'] = 35.65910807942215;
-		$arrPostData['messages'][5]['longitude'] = 139.70372892916203;
+		$arrPostData['messages'][5]['type'] = "location";
+		$arrPostData['messages'][5]['title'] = "my location";
+		$arrPostData['messages'][5]['address'] = "Bangkok, Thailand";
+		$arrPostData['messages'][5]['latitude'] = "35.65910807942215";
+		$arrPostData['messages'][5]['longitude'] = "139.70372892916203';
 	
 	
   }
