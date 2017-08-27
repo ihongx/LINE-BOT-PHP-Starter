7d90b71bd32073cbd95ae5fb96779ad435937638
ihongx <30934502+ihongx@users.noreply.github.com>
2017-08-27 13:04:14 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -62,8 +62,8 @@ if (strpos($_msg, 'สอนเป็ด') !== false) {
 	 $arrPostData['messages'][1]['type'] = 'text';
     $arrPostData['messages'][1]['text'] = 'TEST';
 	 $arrPostData['messages'][2]['type'] = "image";
-    $arrPostData['messages'][2]['originalContentUrl'] = 'https://drive.google.com/file/d/0B6M1dS0Ya-QjenVTZDJLUmdDdGM/view?usp=sharing';
-	$arrPostData['messages'][2]['previewImageUrl'] = 'https://drive.google.com/file/d/0B6M1dS0Ya-QjenVTZDJLUmdDdGM/view?usp=sharing';
+    $arrPostData['messages'][2]['originalContentUrl'] = 'https://raw.githubusercontent.com/ihongx/LINE-BOT-PHP-Starter/master/Capture.JPG';
+	$arrPostData['messages'][2]['previewImageUrl'] = 'https://raw.githubusercontent.com/ihongx/LINE-BOT-PHP-Starter/master/Capture.JPG';
 
 	
 	
