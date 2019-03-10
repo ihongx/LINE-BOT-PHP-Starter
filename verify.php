130ff17dd5793667ba3f3fa15c8880471cc9fce6
ihongx <30934502+ihongx@users.noreply.github.com>
2019-03-10 20:41:10 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,6 +1,6 @@
 <?php
 $access_token = 'lyAGuFf9rLfi1EQ3G9fu9gXojgqm6H1vHnnSIfgajnG5FIHNQkuNuMVmCQsqG+Kj4qxlftBrF3zbJ17KlR7Cm2QDoFf69wuXHp3hFI0mADSS6Jw7K88EQJd2f0k+W5WgL9lLQHLR0OJ6LtbmxnZzUwdB04t89/1O/w1cDnyilFU=';
-$url = 'https://api.line.me/v2/oauth/verify';
+$url = 'https://api.line.me/oauth2/v2.1/verify ';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
